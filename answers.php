05a5c6d1ee390d01524d27d7a5024a18cbccafb2
unknown <elia.mbeppo@trc.co.tz>
2020-07-17 14:08:39 +0300

    HomePage and Answers Backend Implemented

--- answers.php
+++ answers.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+require('config/setup.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,61 +15,153 @@
 		<header class="">
 			<img src="resource/image/logo.png" class="logo">
 
-			<div class="d-flex">
-				<input type="text" name="q" required placeholder="Search question here" class="form-control form-control-sm">
-				<button class="btn btn-sm btn-ghost ml-2">Search</button>
-			</div>
-
 			<div class="nav">
 				<a href="#" class="active">Home</a>
-				<a href="login.php">Login</a>
+				<?php
+				if(isset($_SESSION['id'])){
+				?>
+					<a href="<?php if($_SESSION['role'] === 'admin') { echo 'admin/dashboard.php'; }else echo 'users/home.php'; ?>">Dashboard</a>
+					<a href="logout.php">Logout</a>
+				<?php
+				}else{
+				?>
+					<a href="login.php">Login</a>
+				<?php
+				}
+				?>
 			</div>
 		</header>
 
 
 		<section class="row">		
 			<div class="col-2">
-				<div class="title mb-1">All Categories</div>
+				<div class="title mb-1">Categories</div>
 
 				<div class="d-flex flex-column">
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
+					<?php
+
+						$question_id = mysqli_real_escape_string($con, $_GET['id']);
+
+						if(isset($_POST['reply'])){
+
+							$answer = mysqli_real_escape_string($con, $_POST['answer']);
+
+							if(!empty($answer)){
+
+								$sql = "INSERT INTO answers (user_id, question_id, answers) VALUES ('$_SESSION[id]', '$question_id', '$answer')";
+								$result = mysqli_query($con, $sql);
+
+								if(mysqli_error($con)){
+									echo mysqli_error($con);
+								}
+							}
+						}
+
+						$sql = "SELECT * FROM categories";
+						$result = mysqli_query($con, $sql);
+						if(!mysqli_error($con)){
+							if(mysqli_num_rows($result) > 0){
+
+								echo "<a href='index.php' class='mb-1 p-2'>All categories</a>";
+
+								while($row = mysqli_fetch_assoc($result)){
+									echo "<a href='index.php?category=$row[name]' class='mb-1 p-2'>$row[name]</a>";
+								}
+							}
+						}
+					?>
 				</div>
 			</div>
 			<div class="col-8 border-left">
 				<div>
+					<?php
+						
+						$sql = "SELECT questions.id, question, date, users.username, categories.name FROM questions, users, categories 
+							WHERE questions.user_id = users.id 
+							AND questions.category_id = categories.id
+							AND questions.id = '$question_id'";
+						
+						$result = mysqli_query($con, $sql);
+						if(!mysqli_error($con)){
+							if(mysqli_num_rows($result) > 0){
+
+								while($row = mysqli_fetch_assoc($result)){
+
+									// Count number of answers for each question
+									$count_sql = "SELECT count(*) as totals FROM answers WHERE question_id = '$row[id]'";
+									$count_result = mysqli_query($con, $count_sql);
+									if(!mysqli_error($con)){
+										$total_answers = mysqli_fetch_assoc($count_result)['totals'];
+									}
+									
+					?>
 					<div class="question">
-						<span class="text-success font-weight-bold">john_j1 : </span>
-						<a href="answers.php">
-							An an valley indeed so no wonder future nature vanity. Debating all she mistaken indulged believed provided declared. He many kept on draw lain song as same.
-						</a>
+						<span class="text-success font-weight-bold"><?php echo $row['username']?> : </span>
+						<?php echo $row['question']; ?>
 
 						<div class="details d-flex justify-content-between">
-							<div><small class="text-muted">Answers:</small> 5</div>
-							<div><small class="text-muted">Category:</small> Java</div>
-							<div><small class="text-muted">Posted:</small> 12/12/2020 08:00</div>
+							<div><small class="text-muted">Answers:</small> <?php echo $total_answers; ?></div>
+							<div><small class="text-muted">Category:</small> <?php echo $row['name']; ?></div>
+							<div><small class="text-muted">Posted:</small> <?php echo date('d / m / Y', strtotime($row['date'])); ?></div>
 						</div>
 					</div>
+					<?php
+								}
+							}
+						}
+
+
+						// Check if the user has logged
+						if(isset($_SESSION['id'])){
+					?>
 
 					<div class="my-2">
-						<textarea class="form-control form-control-sm mb-1" name="answer" placeholder="Type an answer here"></textarea>
-						<div class="text-right">
-							<button class="btn btn-sm btn-ghost">Reply</button>
-						</div>
+						<form method="post" action="answers.php?id=<?php echo $question_id; ?>">
+							<textarea class="form-control form-control-sm mb-1" name="answer" rows="5" placeholder="Type an answer here"></textarea>
+							<div class="text-right">
+								<button class="btn btn-sm btn-ghost" type="submit" name="reply">Reply</button>
+							</div>
+						</form>
 					</div>
 
-					<div class="answers">
-						<span class="text-info font-weight-bold">charles : </span>
-						Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery
+					<?php 
+						}
+					?>
+
+					<span class="title mb-2">Answers</span>
+
+					<?php
+
+						$question_id = mysqli_real_escape_string($con, $_GET['id']);
+						
+						$sql = "SELECT answers.id, answers, answers.date, users.username FROM answers, users, questions 
+							WHERE  answers.question_id = questions.id
+							AND answers.user_id = users.id
+							AND answers.question_id = '$question_id'
+							ORDER BY answers.date DESC";
+						
+						$result = mysqli_query($con, $sql);
+						if(!mysqli_error($con)){
+							if(mysqli_num_rows($result) > 0){
+
+								while($row = mysqli_fetch_assoc($result)){
+
+									
+					?>
+
+					<div class="answers mt-3">
+						<span class="text-info font-weight-bold"><?php echo $row['username']; ?> : </span>
+						<?php echo $row['answers']; ?>
 					</div>
 
-					<div class="answers">
-						<span class="text-info font-weight-bold">michael01 : </span>
-						An an valley indeed so no wonder future nature vanity. Debating all she mistaken indulged believed provided declared. He many kept on draw lain song as same.
-					</div>
+					<?php
+							}
+						}else{
+							echo "<div class='alert alert-info mt-3'>No answers provided yet!</div>";
+						}
+					}else echo mysqli_error($con);
+					?>
+
 				</div>
 			</div>
 		</section>
--- index.php
+++ index.php
@@ -16,8 +16,9 @@ require('config/setup.php');
 			<img src="resource/image/logo.png" class="logo">
 
 			<div class="d-flex">
-				<input type="text" name="q" required placeholder="Search question here" class="form-control form-control-sm">
-				<button class="btn btn-sm btn-ghost ml-2">Search</button>
+				<form method="get" action="index.php" id="search_form"></form>
+				<input type="text" name="q" required placeholder="Search question here" class="form-control form-control-sm" form="search_form">
+				<button class="btn btn-sm btn-ghost ml-2" type="submit" name="search" form="search_form">Search</button>
 			</div>
 
 			<div class="nav">
@@ -25,12 +26,12 @@ require('config/setup.php');
 				<?php
 				if(isset($_SESSION['id'])){
 				?>
-				<a href="<?php if($_SESSION['role'] === 'admin') { echo 'admin/dashboard.php'; }else echo 'users/home.php'; ?>">Dashboard</a>
-				<a href="logout.php">Logout</a>
+					<a href="<?php if($_SESSION['role'] === 'admin') { echo 'admin/dashboard.php'; }else echo 'users/home.php'; ?>">Dashboard</a>
+					<a href="logout.php">Logout</a>
 				<?php
 				}else{
 				?>
-				<a href="login.php">Login</a>
+					<a href="login.php">Login</a>
 				<?php
 				}
 				?>
@@ -39,45 +40,88 @@ require('config/setup.php');
 
 
 		<section class="row">		
-			<div class="col-2 border-right">
-				<div class="title mb-1">All Categories</div>
+			<div class="col-2">
+				<div class="title mb-1">Categories</div>
 
 				<div class="d-flex flex-column">
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
-					<a href="index.php" class="mb-1 p-2">Java</a>
+					<?php
+						$sql = "SELECT * FROM categories";
+						$result = mysqli_query($con, $sql);
+						if(!mysqli_error($con)){
+							if(mysqli_num_rows($result) > 0){
+
+								echo "<a href='index.php' class='mb-1 p-2'>All categories</a>";
+
+								while($row = mysqli_fetch_assoc($result)){
+									echo "<a href='index.php?category=$row[name]' class='mb-1 p-2'>$row[name]</a>";
+								}
+							}
+						}
+					?>
 				</div>
 			</div>
 			<div class="col-8 border-left">
 				<div>
+					<?php
+
+						// Check if a category is selected
+						if(isset($_GET['category'])){
+
+							$category = mysqli_real_escape_String($con, $_GET['category']);
+							$sql = "SELECT questions.id, question, date, users.username, categories.name FROM questions, users, categories 
+								WHERE questions.user_id = users.id 
+								AND questions.category_id = categories.id
+								AND categories.name = '$category'";
+
+						}else if(isset($_GET['q'])){
+
+							$question = mysqli_real_escape_String($con, $_GET['q']);
+							$sql = "SELECT questions.id, question, date, users.username, categories.name FROM questions, users, categories 
+								WHERE questions.user_id = users.id 
+								AND questions.category_id = categories.id
+								AND questions.question LIKE '%$question%'";
+
+						}else{
+							// Otherwise if no category is selected
+							$sql = "SELECT questions.id, question, date, users.username, categories.name FROM questions, users, categories 
+								WHERE questions.user_id = users.id 
+								AND questions.category_id = categories.id";
+
+						}
+
+						
+						$result = mysqli_query($con, $sql);
+						if(!mysqli_error($con)){
+							if(mysqli_num_rows($result) > 0){
+
+								while($row = mysqli_fetch_assoc($result)){
+
+									// Count number of answers for each question
+									$count_sql = "SELECT count(*) as totals FROM answers WHERE question_id = '$row[id]'";
+									$count_result = mysqli_query($con, $count_sql);
+									if(!mysqli_error($con)){
+										$total_answers = mysqli_fetch_assoc($count_result)['totals'];
+									}
+									
+					?>
+
 					<div class="question">
-						<span class="text-success font-weight-bold">john_j1 : </span>
-						<a href="answers.php">
-							An an valley indeed so no wonder future nature vanity. Debating all she mistaken indulged believed provided declared. He many kept on draw lain song as same.
-						</a>
+						<span class="text-success font-weight-bold"><?php echo $row['username']?> : </span>
+						<a href="answers.php?id=<?php echo $row['id']; ?>"><?php echo $row['question']; ?></a>
 
 						<div class="details d-flex justify-content-between">
-							<div><small class="text-muted">Answers:</small> 5</div>
-							<div><small class="text-muted">Category:</small> Java</div>
-							<div><small class="text-muted">Posted:</small> 12/12/2020 08:00</div>
+							<div><small class="text-muted">Answers:</small> <?php echo $total_answers; ?></div>
+							<div><small class="text-muted">Category:</small> <?php echo $row['name']; ?></div>
+							<div><small class="text-muted">Posted:</small> <?php echo date('d / m / Y', strtotime($row['date'])); ?></div>
 						</div>
 					</div>
 
-
-					<div class="question">
-						<span class="text-success font-weight-bold">marryann : </span>
-						<a href="answers.php">
-							Delightful unreserved impossible few estimating men favourable see entreaties. She propriety immediate was improving. He or entrance humoured likewise moderate. Much nor game son say feel. Fat make met can must form into gate. Me we offending prevailed discovery
-						</a>
-
-						<div class="details d-flex justify-content-between">
-							<div><small class="text-muted">Answers:</small> 5</div>
-							<div><small class="text-muted">Category:</small> Java</div>
-							<div><small class="text-muted">Posted:</small> 12/12/2020 08:00</div>
-						</div>
-					</div>
+					<?php
+								}
+							}
+						}
+					?>
+					
 				</div>
 			</div>
 		</section>
